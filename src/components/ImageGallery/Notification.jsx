762d7844a143e9c98e5227b18803216484b61bc9
BigSiCorp <si.kocorp@gmail.com>
2022-10-17 13:14:49 +0300

    Note fix try 01

--- src/components/ImageGallery/Notification.jsx
+++ src/components/ImageGallery/Notification.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import React from 'react';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
-export const Note = ()=>{
+// export const Note = ()=>{
    
-      const notify = () => toast("Wow so easy!");
+//       const notify = () => toast("Wow so easy!");
   
-      return (
-        <div>
-          <button onClick={notify}>Notify!</button>
-          <ToastContainer />
-        </div>
-      );
-    }
+//       return (
+//         <div>
+//           <button onClick={notify}>Notify!</button>
+//           <ToastContainer />
+//         </div>
+//       );
+//     }
